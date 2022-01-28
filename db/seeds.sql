5b72bf47ea0a5bd718981c0098c3b1e5026524b6
DrDano <danielharned@gmail.com>
2022-01-28 11:18:59 -0800

    adding department and role tables to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (name)
+VALUES 
+('Accounting'),
+('Operations'),
+('Research & Development'),
+('Transportation'),
+('Human Resources'),
+('Quality Assurance');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Research Analyst', 70000, 3),
+('Manager', 120000, 2),
+('Developer', 80000, 3),
+('Transportation Manager', 65000, 4),
+('Accountant', 70000, 1),
+('Operations Analyst', 75000, 2),
+('Data Manager', 80000, 6),
+('Corporate Counselor', 55000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('')
