ddaa3d294bec338f4bf1b26af8b210735c2589f5
DrDano <danielharned@gmail.com>
2022-01-29 21:56:11 -0800

    adding recursive logic to allow main application run until user desides to terminate it

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const Presenter = require('./presenters/presenter');
 const present = new Presenter;
 const Prompt = require('./controllers/prompts/prompts');
+const Query = require('./service/queries');
+const query = new Query;
 const prompt = new Prompt;
 
 present.setDataModel();
 
-prompt.mainMenu()
-.then((result) => {
-    if (result.menu.split(' ')[0] === 'query') {
-        present.display(result.menu)
-        return;
-    }
-    present.execute(result.menu)
-})
-.then(() => prompt.mainMenu())
+function app() {
+    prompt.mainMenu()
+    .then((result) => {
+        if (result.menu.split(' ')[0] === 'query') {
+            present.display(result.menu)
+            return;
+        } else present.execute(result.menu)
+    })
+    .then(() => {
+        query.disconnect()
+        return app();
+    })
+}
+
+app();
